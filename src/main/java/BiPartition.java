90a102f987b2316274ff06523cbb2f4412bc17dd
Zhiming <xiaoxiaomingbao@gmail.com>
2024-12-17 09:18:52 +0800

    Jenkins problem solved, return to the original state and use JDK 22

--- src/main/java/BiPartition.java
+++ src/main/java/BiPartition.java
@@ -43,7 +43,7 @@ class BiPartition {
         Arrays.fill(fa, -1);
         List<Integer>[] g = new List[n + 1];
         for (int i = 0; i <= n; ++i) {
-            g[i] = new ArrayList<Integer>();
+            g[i] = new ArrayList<>();
         }
         for (int[] p : dislikes) {
             g[p[0]].add(p[1]);
